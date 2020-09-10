5e06bd3f635c01e128ff59f2987f2e4952a28966
ybuchy <yann.buchy@gmx.de>
2020-09-11 01:08:06 +0200

    added: create udp socket on host

--- socket.c
+++ socket.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,46 +14,49 @@ char *get_ip_str(char *hostname) {
 	printf("Failed to get infos about host: %s\n", hostname);
 	return NULL;
     }
-    return inet_ntoa(*((struct in_addr *) host_entry->h_addr));
+    char *ip = (char *) calloc(16, sizeof(char));
+    strncpy(ip, inet_ntoa(*((struct in_addr *) host_entry->h_addr)), 16);
+    return ip;
 }
 
 int main (void) {
-    char hostname[256];
+    char hostname[256] = { 0 };
     if (gethostname(hostname, sizeof(hostname)) == -1) {
-	printf("Failed to get hostname\n");
+	printf("Failed to get hostname");
 	return 1;
     }
 
     char *host_ip = get_ip_str(hostname);
+    if (host_ip == NULL) {
+	printf("Failed to get ip addr of: %s", hostname);
+	return 1;
+    }
     printf("ip: %s\n", host_ip);
 
     printf("Enter hostname of communication partner: ");
-    char comm_hostname[256];
+    char comm_hostname[256] = { 0 };
     fgets(comm_hostname, sizeof(comm_hostname), stdin);
     char *newline = strchr(comm_hostname, '\n');
     if (newline != NULL) *newline = '\0';
 
     char *comm_ip = get_ip_str(comm_hostname);
+    if (comm_ip == NULL) {
+	printf("Failed to get ip addr of: %s", comm_hostname);
+	return 1;
+    }
     printf("ip: %s\n", comm_ip);
 
-    
-    /* int socket_fd = socket(AF_INET, SOCK_RAW, ); */
-
-    /* struct sockaddr_in addr; */
-    /* addr.sin_family = AF_INET; */
-    /* addr.sin_addr.s_addr = inet_addr("192.168.188.23"); */
-    /* addr.sin_port = htons(12345); */
-
-    /* if (bind(socket_fd, (struct sockaddr *) &addr, sizeof(struct sockaddr)) == -1) { */
-    /* 	printf("Failed to bind to the socket! %d", errno); */
-    /* 	return 1; */
-    /* } */
-
-    /* struct sockaddr_in out_addr; */
-    /* out_addr.sin_family = AF_INET; */
-    /* out_addr.sin_addr.s_addr = inet_addr("192.168.188.22"); */
-    /* addr.sin_port = htons(21345); */
-
-    /* int conn_fd = ( */
-
+    int socket_fd = socket(AF_INET, SOCK_DGRAM, 17);
+    if (socket_fd == -1) {
+	printf("Failed to create socketfd %d", errno);
+	return 1;
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(host_ip);
+    addr.sin_port = htons(12345);
+    if (bind(socket_fd, (struct sockaddr *) &addr, sizeof(struct sockaddr)) == -1) {
+    	printf("Failed to bind to the socket for host: %s! %d", host_ip, errno);
+    	return 1;
+    }
 }
